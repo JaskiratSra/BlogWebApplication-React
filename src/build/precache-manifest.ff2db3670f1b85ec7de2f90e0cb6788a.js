9439f8ab1f58977c21ebc37bfc6db99abadd963a
JaskiratSinghSra <35119674+JaskiratSinghSra@users.noreply.github.com>
2019-10-03 14:14:00 -0400

    blog website

---
+++ src/build/precache-manifest.ff2db3670f1b85ec7de2f90e0cb6788a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb4cd49685d2c3b3ec3717fd83bcf55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fde955465b1a58c6f15",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "e2772b5260e8560cffc5",
+    "url": "/static/js/2.fe65b326.chunk.js"
+  },
+  {
+    "revision": "3fde955465b1a58c6f15",
+    "url": "/static/js/main.e7c02d25.chunk.js"
+  },
+  {
+    "revision": "16a05694940ed8d72d10",
+    "url": "/static/js/runtime-main.3da22910.js"
+  }
+]);
